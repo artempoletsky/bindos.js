4042954ced406f4319651fe6556884db3d885240
User <User@User-HP>
2012-10-14 12:28:47 +0400

    lets go

---
+++ src/js/events.js
@@ -0,0 +1,186 @@
+(function(){
+	function Events(){
+		
+	}
+	var eventSplitter=/\s+/;
+	var namespaceSplitter='.';
+	function parse(event)
+	{
+		var arr=(''+event).split(namespaceSplitter);
+		return {
+			n: arr[0], 
+			ns: arr.slice(1),
+			o: event
+		}
+	}
+	function compareNames(arr1,arr2)
+	{
+		for(var i=arr1.length-1;i>=0;i--)
+		{
+			if(!~arr2.indexOf(arr1[i]))
+				return false;
+		}
+		return true;
+	}
+	function compareBinds(bind1,bind2,nsInvert)
+	{
+		console.log(bind1,bind2);
+		console.log(compareNames(bind1.ns,bind2.ns));
+		if(bind1.n && bind1.n != bind2.n)
+			return false;
+		if(bind1.fn && bind1.fn !== bind2.fn)
+			return false;
+		
+		if(bind1.c && bind1.c !== bind2.c)
+			return false;
+		if(bind1.ns.length && !compareNames(bind1.ns,bind2.ns))
+			return false;
+		return true;
+	}
+	function makeBind(event,fn,context)
+	{
+		var bind=parse(event);
+		bind.fn=fn;
+		bind.c=context;
+		return bind;
+	}
+	function add(self,bind){
+		var binds,curBind;
+		
+		binds=self._listeners||{}
+		
+		curBind=binds[bind.n]||[];
+		
+		curBind.push(bind);
+		
+		binds[bind.n]=curBind;
+		
+		self._listeners=binds;
+	}
+	function findBinds(binds,event,fn,context,mode)
+	{
+		var result=[],a,b,bind=makeBind(event,fn,context);
+		if(!mode)
+			mode='filter';
+		
+		for(a in binds)
+		{
+			
+			for(b=binds[a].length-1;b>=0;b--)
+			{
+				//console.log(binds[a][b].ns,bind.ns);
+				//console.log(compareBinds(bind,binds[a][b]));
+				if(compareBinds(bind,binds[a][b]))
+				{
+					if(mode=='filter')
+						result.push(binds[a][b]);
+					else if(mode=='any')
+					{
+						return true;
+					}
+				}
+				else if(mode=='invert')
+				{
+					result.push(binds[a][b]);
+				}
+				
+			}
+		}
+		if(mode!='any')
+			return result;
+		else
+			return false;
+	}
+	function remove(event,fn,context){
+		var bind,binds,i;
+		if(!this._listeners)
+			return;
+		if(!event&&!fn&&!context)
+		{
+			delete this._listeners;
+			return;
+		}
+		
+		bind=makeBind(event,fn,context);
+		
+		if(!bind.ns.length&&!fn&&!context)
+		{
+			delete this._listeners[bind.n];
+			return;
+		}
+		
+		binds=findBinds(this._listeners,event,fn,context,'invert');
+		
+		delete this._listeners;
+		for(i=binds.length-1;i>=0;i--)
+		{
+			add(this,binds[i])
+		}
+	}
+	
+	function keys(obj)
+	{
+		var arr=[];
+		for(var prop in obj)
+		{
+			arr.push(prop);
+		}
+		return arr;
+	}
+	Events.prototype.on=function(events,fn,context){
+		var aEvents=events.split(eventSplitter),i,bind;
+		if(typeof fn != 'function')
+			throw TypeError('function expected');
+		
+		if(!context)
+			context=this;
+		for(i=aEvents.length-1;i>=0;i--)
+		{
+			bind=makeBind(aEvents[i], fn, context);
+			add(this,bind);
+		}
+		return this;
+	}
+	Events.prototype.off=function(events,fn,context){
+		if(!events)
+		{
+			remove.call(this,'',fn,context);
+			return this;
+		}
+		var aEvents=events.split(eventSplitter),i,l;
+		for(i=0,l=aEvents.length;i<l;i++)
+		{
+			remove.call(this,aEvents[i],fn,context)
+		}
+		return this;
+	}
+	Events.prototype.fire=function(events){ 
+		if(!this._listeners)
+			return this;
+		
+		var aEvents,i,j,l,binds,bind,type;
+		aEvents=typeof events == 'string'? events.split(eventSplitter): [events];
+		
+		for(i=0,l=aEvents.length;i<l;i++)
+		{
+			type=typeof aEvents[i] == 'string'? aEvents[i]: aEvents[i].type;
+			
+			binds=findBinds(this._listeners,type,false,false);
+			//console.log(binds);
+			for(j=binds.length-1;j>=0;j--)
+			{
+				bind=binds[j];
+				bind.fn.call(bind.c,aEvents[i]);
+			}
+		}
+		
+		return this;
+	}
+
+	Events.prototype.hasListener=function(event){
+		if(!this._listeners)
+			return false;
+		return findBinds(this._listeners,event,false,false,'any');
+	}
+	window.Events=Events;
+}).call(this);
---
+++ src/js/events.test.js
@@ -0,0 +1,152 @@
+describe('Events', function(){
+	var disp=new Events();
+	
+	describe('support one single event',function(){
+		var spyHandler=jasmine.createSpy('handler');
+		var spyHandler2=jasmine.createSpy('handler');
+		
+		it('and should be able to bind event', function(){
+			expect(disp.hasListener('foo')).toBeFalsy();
+			disp.on('foo', spyHandler);
+			disp.on('bar', spyHandler2);
+			expect(disp.hasListener('foo')).not.toBeFalsy();
+		})
+		it('and should be able to trigger event', function(){
+			disp.fire('foo');
+			expect(spyHandler).toHaveBeenCalled();
+			
+		})
+		
+		it('and should be able to trigger multiple handlers', function(){
+			
+			var context=new Object(),
+			event={
+				type: 'baz'
+			}
+			var obj={
+				bazHandler:function(e){
+					expect(e).toBe(event);
+					expect(this).toBe(context);
+				},
+				bazHandler2:function(e){
+					expect(e).toBe(event);
+					expect(this).toBe(obj);
+				}
+			}
+			
+			spyOn(obj, 'bazHandler');
+			spyOn(obj, 'bazHandler2');
+			
+			disp.on('baz',obj.bazHandler ,context)
+			disp.on('baz',obj.bazHandler2)
+			disp.fire(event);
+			expect(obj.bazHandler.calls.length).toEqual(1);
+			expect(obj.bazHandler2.calls.length).toEqual(1);
+			
+			disp.fire('baz');
+			expect(obj.bazHandler.calls.length).toEqual(2);
+			expect(obj.bazHandler2.calls.length).toEqual(2);
+			
+			disp.fire('baz2');
+			expect(obj.bazHandler.calls.length).toEqual(2);
+			expect(obj.bazHandler2.calls.length).toEqual(2);
+		})
+		
+		it('and should be able to unbind event', function(){
+			//console.log(disp._listeners);
+			disp.off('foo',spyHandler);
+			//console.log(disp._listeners);
+			disp.fire('foo');
+			expect(spyHandler.calls.length).toEqual(1);
+			disp.fire('bar');
+			expect(spyHandler2.calls.length).toEqual(1);
+		})
+		
+		it('and should be able to unbind all handlers with some context', function(){
+			var context1=new Object();
+			var spy1=jasmine.createSpy('spy1');
+			var spy2=jasmine.createSpy('spy2');
+			
+			disp.on('foo', spy2).on('foo', spy1, context1);
+			disp.fire('foo');
+			expect(spy1.calls.length).toEqual(1);
+			expect(spy2.calls.length).toEqual(1);
+			
+			disp.off(null,null,context1);
+			
+			disp.fire('foo');
+			
+			expect(spy1.calls.length).toEqual(1);
+			expect(spy2.calls.length).toEqual(2);
+		})
+		
+		it('and should be able to unbind all same handlers', function(){
+			var spy1=jasmine.createSpy('spy1');
+			var spy2=jasmine.createSpy('spy2');
+			
+			disp.on('foo', spy1).on('foo', spy2);
+			disp.fire('foo');
+			expect(spy1.calls.length).toEqual(1);
+			expect(spy2.calls.length).toEqual(1);
+			disp.off(null,spy1);
+			disp.fire('foo');
+			expect(spy1.calls.length).toEqual(1);
+			expect(spy2.calls.length).toEqual(2);
+		})
+	})
+	//var button=document.createElement('button');
+	//$(button).on()
+	describe('support name spaces', function(){
+		var disp=new Events();
+		it('and should trigger by namespace', function(){
+			var spy1=jasmine.createSpy('spy1');
+			var spy2=jasmine.createSpy('spy2');
+			disp.on('foo',spy1);
+			disp.on('foo.name',spy2);
+			
+			disp.fire('foo');
+			expect(spy1.calls.length).toEqual(1);
+			expect(spy2.calls.length).toEqual(1);
+			
+			disp.fire('foo.name');
+			expect(spy1.calls.length).toEqual(1);
+			expect(spy2.calls.length).toEqual(2);
+			return;
+			disp.fire('foo.another');
+			expect(spy1.calls.length).toEqual(2);
+			expect(spy2.calls.length).toEqual(1);
+			
+			var spy3=jasmine.createSpy('spy3');
+			disp.on('bar.foo',spy3);
+			
+			disp.fire('bar.foo');
+			expect(spy3.calls.length).toEqual(1);
+			disp.fire('foo.name bar.foo');
+			expect(spy1.calls.length).toEqual(4);
+			expect(spy2.calls.length).toEqual(3);
+			expect(spy3.calls.length).toEqual(2);
+			//*/
+		})
+		
+		it('and should unbind by namespace', function(){
+			var spy1=jasmine.createSpy('spy1');
+			var spy2=jasmine.createSpy('spy2');
+			var spy3=jasmine.createSpy('spy2');
+			
+			disp.on('foo.bar',spy3);
+			disp.on('foo.name',spy1);
+			disp.on('foo',spy2);
+			
+			disp.off('foo.name');
+			disp.fire('foo');
+			
+			expect(spy1.calls.length).toEqual(0);
+			expect(spy2.calls.length).toEqual(1);
+			expect(spy3.calls.length).toEqual(1);
+			
+			disp.off('.bar');
+			
+		})
+		
+	})
+})
