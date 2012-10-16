0df8aa5ab8cc946adc88b762fc96317656acd0fb
User <User@User-HP>
2012-10-16 21:25:00 +0400

    vm 3

--- spec/viewmodel.test.js
+++ spec/viewmodel.test.js
@@ -19,18 +19,22 @@ describe('ViewModel', function(){
 	it('can create VM object with constructor', function(){
 		var init=jasmine.createSpy('init');
 		var click=jasmine.createSpy('click');
-		
-		ViewModel.create({
-			el: 'div',
-			initialize: init,
-			events: {
+		function TestViewModel(){
+			this.el='em';
+			this.initialize=init;
+			this.events={
 				'click': 'onClick'
-			},
-			onClick: click
-		});
+			};
+			this.onClick= click;
+			ViewModel.call(this);
+		}
+		TestViewModel.prototype=new ViewModel();
+		var vm=new TestViewModel();
+		expect(vm.events.click).toEqual('onClick');
 		expect(init).toHaveBeenCalled();
 		expect(click).not.toHaveBeenCalled();
-		
+		vm.$el.click();
+		expect(click).toHaveBeenCalled();
 	})
 	
 	it('each method must return this', function(){
--- src/js/viewmodel.js
+++ src/js/viewmodel.js
@@ -10,12 +10,16 @@
 		return newObj._construct();
 	}
 	function ViewModel() {
-		Events.call(this);
-		this._binds={};
-		this.events={};
-		this._cid='';
-		this.$el;
-		this.el;
+		//для подсказок
+		if(false)
+		{
+			Events.call(this);
+			this._binds={};
+			this.events={};
+			this._cid='';
+			this.$el=$();
+			this.el=document.createElement('div');
+		}
 		this._construct();
 	}
 	ViewModel.prototype=new Events();
@@ -47,7 +51,6 @@
 			this.el=document.createElement(this.el);
 		}
 		this.$el=$(this.el);
-		
 		this.parse();
 		this.delegateEvents();
 		this.initialize();
@@ -56,11 +59,13 @@
 	ViewModel.prototype.initialize=function(){}
 	ViewModel.prototype.delegateEvents=function(events){
 		events||(events=this.events);
+		
 		var fnName,fn,name,eventsPath,eventName,me=this;
 		for(name in events)
 		{
+			
 			fnName=events[name];
-			fn=this[fnName];
+			fn=me[fnName];
 			if(typeof fn != 'function')
 				throw TypeError(fnName+' is not a function');
 			eventsPath=name.split(eventSplitter);
@@ -70,11 +75,11 @@
 			}
 			if(eventsPath.length)
 			{
-				this.$el.delegate(eventName,eventName,proxy);
+				me.$el.delegate(eventsPath.join(' '),eventName,proxy);
 			}
 			else
 			{
-				this.$el.bind(eventName,proxy);
+				me.$el.bind(eventName,proxy);
 			}
 		}
 		return this;
@@ -84,7 +89,9 @@
 		return this;
 	}
 	
-	ViewModel.prototype.render=function(){return this;};
+	ViewModel.prototype.render=function(){
+		return this;
+	};
 	this.ViewModel=ViewModel;
 }).call(this);
 
