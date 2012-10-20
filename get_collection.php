d9e04234d81b4200193c5fae849a697a42d9e47f
uistler <uistler@gmail.com>
2012-10-20 11:25:29 +0400

    collection generator

--- get_collection.php
+++ get_collection.php
@@ -1,7 +1,12 @@
 <?php
+$value = array();
+for ($index = 0; $index < 100; $index++)
+{
+	$value[$index]=array(
+		'id'=> $index,
+		'amount'=>  rand(10, 1500),
+	);
+}
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+$json = json_encode($value);
+echo $json;
