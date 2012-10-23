9b5bdb946fbe37926072e1c31703ddeaf2970b46
poletsky <poletsky@immo.ru>
2012-10-23 17:34:49 +0400

    Полевые условия либа не прошла :((((

--- bin/frontbone.js
+++ bin/frontbone.js
@@ -392,7 +392,8 @@
 		}
 	
 		var Computed=function(fn,context){
-		
+			if(!context)
+				context=this;
 			var value=fn.call(context);
 		
 			var resfn=function(){
@@ -431,203 +432,31 @@
 		this.Computed=Computed;
 		this.Subscribeable=Subscribeable;
 	}).call(this);
-
-	(function(){
-		var $=this.$;
 	
-		var eventSplitter=/\s+/;
-		var bindSplitter=/\s*;\s*/;
-		var simpleTagRegex=/^[a-z]+$/;
 	
-		function findBinds(element,vm,context)
-		{
-			ViewModel.findBinds(element,vm,context);
-		}
-		ViewModel.findObservable=function(context,string){
-			var comp=Computed(function(){
-				try {
-				
-					with(Observable.isObservable(context)?context():context)
-						return eval(string);
-				} catch (exception) { 
-					throw 'Error "'+exception.message+'" in expression "'+string+'"';
-				}
-
-			
-			},context);
-		
-			return comp;
-		}
-		ViewModel.findBinds=function(element,context){
-			var children,curBindsString,binds,i,newctx;
-			//console.dir(element);
-			curBindsString=element.attributes&&element.attributes['data-bind'];
-			//console.log(curBindsString);
-		
-			if(curBindsString)
-			{
-				binds=curBindsString.value.split(bindSplitter);
-				for(i=binds.length-1;i>=0;i--){
-				
-					var arr=binds[i].match(/^\s*(\S+?)\s*:\s*(\S[\s\S]*\S)\s*$/);
-				
-					var fn=ViewModel.binds[arr[1]];
-				
-					if(fn)
-					{
-						newctx=fn.call(this, element, arr[2], context);
-						if(newctx===false)
-						{
-							return;
-						}
-						else if(newctx)
-						{
-							context=newctx;
-						}
-						
-					}
-				
-				}
-			}
-			children=element.childNodes;
-			if(children)
-				for(i=children.length-1;i>=0;i--){
-					findBinds(children[i],context);
-				}
-		}
-		/**
-	 * @return ViewModel
-	 */
-		ViewModel.create=function(obj){
-			var newObj={};
-			$.extend(newObj,ViewModel.prototype,obj);
-			//console.log(newObj.initialize);
-			return newObj._construct();
-		}
-		function ViewModel() {
-			//для подсказок
-			if(false)
-			{
-				Events.call(this);
-				this._binds={};
-				this.events={};
-				this.attributes={};
-				this._cid='';
-				this.$el=$();
-				this.el=document.createElement('div');
-			}
-			this._construct();
-		}
-		ViewModel.prototype=new Events();
-		
-		ViewModel.prototype.setElement=function(el){
-			this.undelegateEvents();
-			this.el=el;
-			this.$el=$(el);
-			this.parse().delegateEvents();
-			return this;
-		}
-		ViewModel.prototype.parse=function(){
-			delete this._binds;
-			findBinds(this.el, this, '');
-			return this;
-			var $el,binds,bind,me=this,i,context=me;
-			me.$el.find('[data-bind]').add(me.$el).filter('[data-bind]').each(function(){
-				$el=$(this);
-				binds=$el.attr('data-bind').split(bindSplitter);
-				for(i=binds.length-1;i>=0;i--){
-				
-					var arr=binds[i].match(/^\s*(\S+?)\s*:\s*(\S[\s\S]*\S)\s*$/);
-					//console.log(binds[i]);
-					//console.log(arr);
-					//console.log(binds[i].match(/^(\S+):/));
-				
-					var fn=ViewModel.binds[arr[1]];
-				
-					if(fn)
-					{
-						fn.call(me, this,arr[2], context)	
-					}
-				
-				//bind=parseBind(binds);
-				//me.addBind(bind.a, bind.b);
-				}
-			})
-			return this;
-		}
 	
-		ViewModel.prototype._construct=function(){
-			var me=this;
-			if(!me._cid)
-			{
-				me._cid=VM.unique('vm');
-			}
-		
-			if(!me.el)
-				me.el='div';
-			if(typeof me.el == 'string')
-			{
-				if(simpleTagRegex.test(me.el))
-					me.el=document.createElement(me.el);
-				else
-					me.el=$(me.el)[0];
-			}
-			me.$el=$(me.el);
-			me.$=function(selector){
-				return me.$el.find(selector);
-			}
-			me.initialize();
-		
-			if(me.autoinit)
-				me.parse().delegateEvents();
-			return this;
-		}
-		ViewModel.prototype.autoinit=true;
-		ViewModel.prototype.initialize=function(){}
-		ViewModel.prototype.delegateEvents=function(events){
-			events||(events=this.events);
-			this.undelegateEvents();
-			var fnName,fn,name,eventsPath,eventName,me=this;
-			for(name in events)
-			{
-			
-				fnName=events[name];
-				fn=me[fnName];
-				if(typeof fn != 'function')
-					throw TypeError(fnName+' is not a function');
-				eventsPath=name.split(eventSplitter);
-				eventName=eventsPath.shift()+'.'+this._cid;
-			
-				var proxy=(function(fn){
-					return function(){
-						fn.apply(me,arguments);
-					}
-				})(fn);
-				if(eventsPath.length)
-				{
-					me.$el.delegate(eventsPath.join(' '),eventName,proxy);
-				}
-				else
-				{
-					me.$el.bind(eventName,proxy);
-				}
-			}
-			return this;
-		}
-		ViewModel.prototype.undelegateEvents=function(){
-			this.$el.unbind('.'+this._cid);
-			return this;
-		}
-	
-		ViewModel.prototype.render=function(){
-			return this;
-		};
-		this.ViewModel=ViewModel;
-	}).call(this);
-
 	(function(){
 		var modelsMap={};
 		function Model(json,options){
+			this._construct(json, options);
+		}
+		Model.extend=function(props){
+			var ParentClass=this.prototype.constructor;
+			var Constructor=function(){
+				this._construct.apply(this, arguments);
+			}
+			Constructor.prototype=new ParentClass();
+			Constructor.prototype.constructor=Constructor;
+			for(var prop in props)
+			{
+				Constructor.prototype[prop]=props[prop];
+			}
+			Constructor.extend=ParentClass.extend;
+			return Constructor;
+		}
+		Model.prototype=new Events();
+		Model.prototype.constructor=Model;
+		Model.prototype._construct=function(json,options){
 			json||(json={});
 			options||(options={});
 			this.attributes=this.parse(json);
@@ -642,22 +471,6 @@
 			this.url='api/?model='+this.mapping+'&id='+this.id;
 			this.initialize();
 		}
-		Model.extend=function(props){
-			var ParentClass=this.prototype.constructor;
-			var Constructor=function(json,options){
-				ParentClass.call(this,json,options)
-			}
-			Constructor.prototype=new ParentClass();
-			Constructor.prototype.constructor=Constructor;
-			for(var prop in props)
-			{
-				Constructor.prototype[prop]=props[prop];
-			}
-			Constructor.extend=ParentClass.extend;
-			return Constructor;
-		}
-		Model.prototype=new Events();
-		Model.prototype.constructor=Model;
 		Model.prototype.idAttribute='id';
 		Model.prototype.mapping=false;
 		Model.prototype.initialize=function(){
@@ -736,6 +549,229 @@
 		}
 		this.Model=Model;
 	})();
+	
+	(function(){
+		var $=this.$;
+	
+		var eventSplitter=/\s+/;
+		var bindSplitter=/\s*;\s*/;
+		var simpleTagRegex=/^[a-z]+$/;
+	
+		function findBinds(element,vm,context)
+		{
+			ViewModel.findBinds(element,vm,context);
+		}
+		ViewModel.extend=Model.extend;
+		ViewModel.findObservable=function(context,string){
+			var comp=Computed(function(){
+				try {
+				
+					with(Observable.isObservable(context)?context():context)
+						return eval(string);
+				} catch (exception) { 
+					throw 'Error "'+exception.message+'" in expression "'+string+'"';
+				}
+
+			
+			},context);
+		
+			return comp;
+		}
+		ViewModel.findBinds=function(element,context){
+			var children,curBindsString,binds,i,newctx;
+			//console.dir(element);
+			curBindsString=element.attributes&&element.attributes['data-bind'];
+			//console.log(curBindsString);
+		
+			if(curBindsString)
+			{
+				binds=curBindsString.value.split(bindSplitter);
+				for(i=binds.length-1;i>=0;i--){
+				
+					var arr=binds[i].match(/^\s*(\S+?)\s*:\s*(\S[\s\S]*\S)\s*$/);
+				
+					var fn=ViewModel.binds[arr[1]];
+				
+					if(fn)
+					{
+						newctx=fn.call(this, element, arr[2], context);
+						if(newctx===false)
+						{
+							return;
+						}
+						else if(newctx)
+						{
+							context=newctx;
+						}
+						
+					}
+				
+				}
+			}
+			children=element.childNodes;
+			if(children)
+				for(i=children.length-1;i>=0;i--){
+					findBinds(children[i],context);
+				}
+		}
+		/**
+	 * @return ViewModel
+	 */
+		ViewModel.create=function(obj){
+			var newObj={};
+			$.extend(newObj,ViewModel.prototype,obj);
+			//console.log(newObj.initialize);
+			return newObj._construct();
+		}
+		function ViewModel(options) {
+			//для подсказок
+			if(false)
+			{
+				Events.call(this);
+				this._binds={};
+				this.events={};
+				this.attributes={};
+				this._cid='';
+				this.$el=$();
+				this.el=document.createElement('div');
+			}
+			this._construct();
+		}
+		ViewModel.prototype=new Events();
+		ViewModel.prototype.constructor=ViewModel;
+		ViewModel.prototype.setElement=function(el){
+			this.undelegateEvents();
+			this.el=el;
+			this.$el=$(el);
+			this.parse().delegateEvents();
+			return this;
+		}
+		ViewModel.prototype.parse=function(){
+			delete this._binds;
+			findBinds(this.el, this, '');
+			return this;
+			var $el,binds,bind,me=this,i,context=me;
+			me.$el.find('[data-bind]').add(me.$el).filter('[data-bind]').each(function(){
+				$el=$(this);
+				binds=$el.attr('data-bind').split(bindSplitter);
+				for(i=binds.length-1;i>=0;i--){
+				
+					var arr=binds[i].match(/^\s*(\S+?)\s*:\s*(\S[\s\S]*\S)\s*$/);
+					//console.log(binds[i]);
+					//console.log(arr);
+					//console.log(binds[i].match(/^(\S+):/));
+				
+					var fn=ViewModel.binds[arr[1]];
+				
+					if(fn)
+					{
+						fn.call(me, this,arr[2], context)	
+					}
+				
+				//bind=parseBind(binds);
+				//me.addBind(bind.a, bind.b);
+				}
+			})
+			return this;
+		}
+		ViewModel.prototype.bindToModel=function(json){
+			var model=Observable(new Model(json));
+			
+			if(!this._bindedToModel)
+				for(var prop in model().attributes)
+				{
+					this[prop]=(function(prop,context){
+						
+						return Computed(function(){
+							
+							var mod=model();
+							if(!mod)
+							{
+								return '';
+							}
+							var val=(this['format_'+prop])? this['format_'+prop](mod.get(prop)):mod.get(prop);
+							//console.log(prop,val);
+							return val;
+							
+						}, context)
+					})(prop,this);
+				}
+			this._bindedToModel=true;
+			return model;
+		}
+		ViewModel.prototype._construct=function(options){
+			options||(options={});
+			this.collection=options.collection;
+			this.model=options.model;
+			var me=this;
+			if(!me._cid)
+			{
+				me._cid=VM.unique('vm');
+			}
+		
+			if(!me.el)
+				me.el='div';
+			if(typeof me.el == 'string')
+			{
+				if(simpleTagRegex.test(me.el))
+					me.el=document.createElement(me.el);
+				else
+					me.el=$(me.el)[0];
+			}
+			me.$el=$(me.el);
+			me.$=function(selector){
+				return me.$el.find(selector);
+			}
+			me.initialize();
+		
+			if(me.autoinit)
+				me.parse().delegateEvents();
+			return this;
+		}
+		ViewModel.prototype.autoinit=true;
+		ViewModel.prototype.initialize=function(){}
+		ViewModel.prototype.delegateEvents=function(events){
+			events||(events=this.events);
+			this.undelegateEvents();
+			var fnName,fn,name,eventsPath,eventName,me=this;
+			for(name in events)
+			{
+			
+				fnName=events[name];
+				fn=me[fnName];
+				if(typeof fn != 'function')
+					throw TypeError(fnName+' is not a function');
+				eventsPath=name.split(eventSplitter);
+				eventName=eventsPath.shift()+'.'+this._cid;
+			
+				var proxy=(function(fn){
+					return function(){
+						fn.apply(me,arguments);
+					}
+				})(fn);
+				if(eventsPath.length)
+				{
+					me.$el.delegate(eventsPath.join(' '),eventName,proxy);
+				}
+				else
+				{
+					me.$el.bind(eventName,proxy);
+				}
+			}
+			return this;
+		}
+		ViewModel.prototype.undelegateEvents=function(){
+			this.$el.unbind('.'+this._cid);
+			return this;
+		}
+	
+		ViewModel.prototype.render=function(){
+			return this;
+		};
+		this.ViewModel=ViewModel;
+	}).call(this);
+
+	
 
 	(function(){
 		function Collection(json){
