7319e1422af86218edd7b8a5a69ced5203e3e8e7
uistler <uistler@gmail.com>
2013-01-26 14:40:29 +0400

    Model refactoring

--- src/js/model.js
+++ src/js/model.js
@@ -1,111 +1,159 @@
 (function(){
 	var modelsMap={};
-	function Model(json,options){
-		json||(json={});
-		options||(options={});
-		this.attributes=this.parse(json);
-		this.id=this.attributes[this.idAttribute];
-		this.cid=VM.unique('c');
-		
-		if(this.mapping&&this.id)
-		{
-			modelsMap[this.mapping]||(modelsMap[this.mapping]={});
-			modelsMap[this.mapping][this.id]=this;
-		}
-		this.url='api/?model='+this.mapping+'&id='+this.id;
-		this.initialize();
-	}
-	Model.extend=function(props){
-		var ParentClass=this.prototype.constructor;
-		var Constructor=function(json,options){
-			ParentClass.call(this,json,options)
-		}
-		Constructor.prototype=new ParentClass();
-		Constructor.prototype.constructor=Constructor;
-		for(var prop in props)
-		{
-			Constructor.prototype[prop]=props[prop];
-		}
-		Constructor.extend=ParentClass.extend;
-		return Constructor;
-	}
-	Model.prototype=new Events();
-	Model.prototype.constructor=Model;
-	Model.prototype.idAttribute='id';
-	Model.prototype.mapping=false;
-	Model.prototype.initialize=function(){
-		return this;
-	}
-	Model.prototype.parse=function(json){
-		return json;
-	}
-	Model.prototype.update=function(json){
-		this.set(this.parse(json));
-	}
-	Model.prototype.get=function(name){
-		return this.attributes[name];
-	}
-	Model.prototype.validate=function(){
-		return true;
-	}
-	Model.prototype.save=function(){
-		var me=this;
-		if(this.id)
-			VM.sync('PUT', this.url,{
-				data: this.attributes
-			});
-		else
-			VM.sync('POST', this.url,{
-				data: this.attributes,
-				success: function(data){
-					me.set('id',data);
-					me.id=data;
-				}
-			});
-		return true;
-	}
-	Model.prototype.remove=function(){
-		this.fire('remove');
-		if(this.id)
-			VM.sync('DELETE', this.url);
-	}
-	Model.prototype.set=function(name,value){
-		var attrs={},prop;
-		if(arguments.length>1)
-			attrs[name]=value;
-		else
-			attrs=name;
-		var changed={};
-		for(prop in attrs)
-		{
-			this.attributes[prop]=attrs[prop];
-			changed[prop]=attrs[prop];
-			this.fire('change:'+prop);
-		}
-		this.fire({
-			type: 'change',
-			changed:changed
-		});
-		return this;
-	}
-	Model.fromStorage=function(name,id){
-		modelsMap[name]||(modelsMap[name]={});
-		return modelsMap[name][id];
-	}
-	Model.createOrUpdate=function(constuctor,json){
-		var proto=constuctor.prototype,fromStorage,idAttr,parsed,id;
-		if(proto.mapping)
-		{
-			idAttr=proto.idAttribute;
-			parsed=proto.parse(json);
-			fromStorage=Model.fromStorage(proto.mapping, parsed[idAttr]);
-			if(fromStorage)
+	
+	var Model=Events.extend({
+		constructor: function(data,options){
+			data||(data={});
+			options||(options={});
+			this.attributes=_.extend({},this.defaults,this.parse(data));
+			this._changed={};
+			this.id=this.attributes[this.idAttribute];
+			this.cid=_.unique('c');
+			//заносим в глобальную коллекцию
+			if(this.mapping&&this.id)
 			{
+				modelsMap[this.mapping]||(modelsMap[this.mapping]={});
+				modelsMap[this.mapping][this.id]=this;
+			}
+			this.initialize();
+		},
+		initialize: function(){
+			return this;
+		},
+		idAttribute: 'id',
+		mapping: false,
+		defaults: {},
+		toJSON: function(){
+			return _.clone(this.attributes);
+		},
+		parse: function(json) {
+			return json;
+		},
+		baseURL: '/',
+		url: function(){
+			var mapping=this.mapping||'';
+			return this.baseURL+mapping+'/'+this.id;
+		},
+		update: function(json){
+			this.set(this.parse(json));
+			this._changed = {};
+			return this;
+		},
+		prop: function(key){
+			if(arguments.length==1&&typeof key=='string')
+			{
+				return this.attributes[key];
+			}
+			var values={};
+			if(typeof key=='string')
+				values[key]=arguments[1];
+			else
+				values=key;
+			var self=this;
+			_.each(values,function(val,key){
+				self._changed[key]=self.attributes[key]=val;
+				self.fire('change:'+key);
+			});
+			this.fire('change');
+			return this;
+		},
+		/**
+		 * DEPRECATED since 26.01.2013
+		 */
+		get: function(key){
+			return this.prop.apply(this, arguments);
+		},
+		/**
+		 * DEPRECATED since 26.01.2013
+		 */
+		set: function(){
+			return this.prop.apply(this, arguments);
+		},
+		validate : function() {
+			return true;
+		},
+		fetch: function(options){
+			var me = this;
+			options || (options = {});
+			var opt = {
+				success: function(data) {
+					me.update(data);
+					if(typeof options.success == 'function') {
+						options.success.apply(me, arguments);
+					}
+				},
+				error: function() {
+					if(typeof options.error == 'function') {
+						options.error.apply(me, arguments);
+					}
+				}
+			};
+			var resOpt = _.extend({}, options, opt);
+			Model.sync('read', this.url(), resOpt);
+			return this;
+		},
+		save: function(){
+			var me = this;
+			if(this.id) {
+				Model.sync('update', this.url(), {
+					data: this._changed,
+					success: function(data) {
+						me.update(data);
+					}
+				});
+			} else {
+				Model.sync('create', this.url(), {
+					data: this.attributes,
+					success: function(data) {
+						me.update(data);
+					}
+				});
+			}
+			return this;
+		},
+		remove: function() {
+			this.fire('remove');
+			if(this.id) {
+				VM.sync('delete', this.url());
+			}
+		}
+	});
+	
+	Model.fromStorage = function(name, id) {
+		modelsMap[name] || (modelsMap[name] = {});
+		return modelsMap[name][id];
+	};
+	Model.createOrUpdate = function(constuctor, json) {
+		var proto = constuctor.prototype, fromStorage, idAttr, parsed, id;
+		if(proto.mapping) {
+			idAttr = proto.idAttribute;
+			parsed = proto.parse(json);
+			fromStorage = Model.fromStorage(proto.mapping, parsed[idAttr]);
+			if(fromStorage) {
 				fromStorage.update(json);
 				return fromStorage;
 			}
 		}
 		return new constuctor(json);
 	}
-	this.Model=Model;
-})()
+	
+	Model.sync=function(method,url,options){
+		options||(options={});
+		var data={
+			method: method
+		}
+		if(method=='PUT')
+			method='POST';
+		$.extend(data, options.data);
+		$.ajax({
+			url: url,
+			dataType: 'json',
+			type: method,
+			data: data,
+			success: options.success,
+			error: options.error
+		})
+	}
+	
+	this.Model = Model;
+})();
